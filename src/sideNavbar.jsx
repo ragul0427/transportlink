a6c339710171c71e93c16ce64a191e9d130c0bee
ragul0427 <ragulhp2704@gmail.com>
2023-10-08 21:39:45 +0530

    rtbr

--- src/sideNavbar.jsx
+++ src/sideNavbar.jsx
@@ -50,131 +50,131 @@ function SideNavbar() {
 
   return (
     <div className="w-[14vw] h-screen !z-50 bg-[--secondary-color] ">
-      <div className="w-[14vw] h-screen  lg:border-r lg:border-slate-200 fixed bg-[--primary-color]">
-        <div className="text-center !z-50 flex text-xl border-b border-gray-100 font-bold items-center  justify-around  text-[--primary-color]   !bg-[--secondary-color] h-[9vh]   w-screen py-3">
-          <h1 className="text-[12px] lg:text-xl xsm:pl-1 lg:pl-0 flex items-center justify-center">
-            <Image src={logo} className="!w-14 !h-8 lg:!w-24 lg:!h-12"/>
-          </h1>
+    <div className="w-[14vw] h-screen  lg:border-r lg:border-slate-200 fixed bg-[--primary-color]">
+      <div className="text-center !z-50 flex text-xl border-b border-gray-100 font-bold items-center  justify-around  text-[--primary-color]   !bg-[--secondary-color] h-[9vh]   w-screen py-3">
+        <h1 className="text-[12px] lg:text-xl xsm:pl-1 lg:pl-0 flex items-center justify-center">
+          <Image src={logo} className="!w-14 !h-8 lg:!w-24 lg:!h-12"/>
+        </h1>
+        <div className="hidden lg:block">
+          {location.pathname.split("/")[1]
+            ? location.pathname.split("/")[1]
+            : "Dashboard"}
+        </div>
+        <div className="text-[12px] lg:text-xl">
+          Vehicle Transport Management System
+        </div>
+        <div
+          onMouseOver={() => {
+            setOpen(true);
+          }}
+          onMouseLeave={() => {
+            setOpen(false);
+          }}
+          className="mr-4 cursor-pointer"
+        >
           <div className="hidden lg:block">
-            {location.pathname.split("/")[1]
-              ? location.pathname.split("/")[1]
-              : "Dashboard"}
+            <Avatar
+              style={{
+                backgroundColor: "red",
+              }}
+              size="default"
+            >
+              T
+            </Avatar>
           </div>
-          <div className="text-[12px] lg:text-xl">
-            Vehicle Transport Management System
+          <div className="lg:hidden">
+            <Avatar
+              style={{
+                backgroundColor: "red",
+              }}
+              size="small"
+            >
+              T
+            </Avatar>
           </div>
           <div
-            onMouseOver={() => {
-              setOpen(true);
-            }}
-            onMouseLeave={() => {
-              setOpen(false);
-            }}
-            className="mr-4 cursor-pointer"
+            className={`absolute h-[2vh] w-[10vw] lg:h-[4.5vh] lg:w-[6vw] top-12 left-[85vw] lg:top-4 lg:left-[91vw]  border-b bg-white rounded-md text-[--secondary-color] flex items-center justify-center ${
+              open ? "flex" : "hidden"
+            }`}
           >
-            <div className="hidden lg:block">
-              <Avatar
-                style={{
-                  backgroundColor: "red",
-                }}
-                size="default"
-              >
-                T
-              </Avatar>
-            </div>
-            <div className="lg:hidden">
-              <Avatar
-                style={{
-                  backgroundColor: "red",
-                }}
-                size="small"
-              >
-                T
-              </Avatar>
-            </div>
-            <div
-              className={`absolute   h-[4.5vh] w-[6vw] top-4 left-[91vw]  border-b bg-white rounded-md text-[--secondary-color] flex items-center justify-center ${
-                open ? "flex" : "hidden"
-              }`}
-            >
-              <button
-                className="text-[16px] "
-                onClick={() => {
-                  setModal(!modal);
-                }}
-              >
-                Logout
-              </button>
-            </div>
-          </div>
-        </div>
-        <Modal
-          open={modal}
-          footer={false}
-          closable={false}
-          width={400}
-          className="!py-40"
-        >
-          <p className="text-slate-600 font-bold">
-            Are You sure Want to Logout ?
-          </p>
-          <div className="flex gap-2 items-end justify-end  pt-2">
             <button
-              onClick={handleLogout}
-              className="bg-[--secondary-color] flex items-center justify-center text-white w-[4vw] h-[4vh] rounded-md "
-            >
-              Yes
-            </button>
-            <button
+              className="text-[8px] lg:text-[16px] "
               onClick={() => {
                 setModal(!modal);
               }}
-              className="bg-[--secondary-color] flex items-center justify-center text-white w-[3vw] h-[4vh] rounded-md "
             >
-              no
+              Logout
             </button>
           </div>
-        </Modal>
-        <p
-          className="lg:hidden bg-white border-b border-slate-200 w-[100vw] pl-1"
-          onClick={() => {
-            setMenu(true);
-          }}
-          
-        >
-          <MenuOpenIcon />
+        </div>
+      </div>
+      <Modal
+        open={modal}
+        footer={false}
+        closable={false}
+        width={400}
+        className="!py-40"
+      >
+        <p className="text-slate-600 font-bold">
+          Are You sure Want to Logout ?
         </p>
-        <Drawer
-          open={menu}
-          width={200}
-          destroyOnClose
-          footer={false}
-          placement="left"
-          closeIcon={false}
-          onClose={() => {
-            setMenu(false);
-          }}
-          className="absolute top-[9vh]"
-        >
-          <Menu
-            items={items}
-            defaultSelectedKeys={[localStorage.getItem("selectedKey")]}
-            defaultOpenKeys={["sub1", "sub2", "sub3"]}
-            mode="inline"
-            onClick={onClick}
-            className="absolute left-0 top-0"
-          ></Menu>
-        </Drawer>
+        <div className="flex gap-2 items-end justify-end  pt-2">
+          <button
+            onClick={handleLogout}
+            className="bg-[--secondary-color] flex items-center justify-center px-2 text-white lg:w-[4vw] lg:px-0 lg:h-[4vh] rounded-md "
+          >
+            Yes
+          </button>
+          <button
+            onClick={() => {
+              setModal(!modal);
+            }}
+            className="bg-[--secondary-color] flex items-center justify-center text-white px-2 lg:px-0 lg:w-[3vw] lg:h-[4vh] rounded-md "
+          >
+            no
+          </button>
+        </div>
+      </Modal>
+      <p
+        className="lg:hidden bg-white border-b border-slate-200 w-[100vw] pl-1"
+        onClick={() => {
+          setMenu(true);
+        }}
+        
+      >
+        <MenuOpenIcon />
+      </p>
+      <Drawer
+        open={menu}
+        width={200}
+        destroyOnClose
+        footer={false}
+        placement="left"
+        closeIcon={false}
+        onClose={() => {
+          setMenu(false);
+        }}
+        className="absolute top-[9vh]"
+      >
         <Menu
           items={items}
           defaultSelectedKeys={[localStorage.getItem("selectedKey")]}
           defaultOpenKeys={["sub1", "sub2", "sub3"]}
           mode="inline"
           onClick={onClick}
-          className="hidden lg:flex lg:flex-col"
+          className="absolute left-0 top-0"
         ></Menu>
-      </div>
+      </Drawer>
+      <Menu
+        items={items}
+        defaultSelectedKeys={[localStorage.getItem("selectedKey")]}
+        defaultOpenKeys={["sub1", "sub2", "sub3"]}
+        mode="inline"
+        onClick={onClick}
+        className="hidden lg:flex lg:flex-col"
+      ></Menu>
     </div>
+  </div>
   );
 }
 
